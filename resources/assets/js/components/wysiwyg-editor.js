21fd3c9095f6979b17b93eb75294160582398bf4
sa.almarshud <sa.almarshud@qu.edu.sa>
2018-03-07 22:52:29 +0300

    add arabic KeyBoard

--- resources/assets/js/components/wysiwyg-editor.js
+++ resources/assets/js/components/wysiwyg-editor.js
@@ -3,6 +3,8 @@ class WysiwygEditor {
     constructor(elem) {
         this.elem = elem;
         this.options = require("../pages/page-form");
+        this.options.language = 'ar';
+        this.options.directionality = 'rtl';
         tinymce.init(this.options);
     }
 
